d341ad0336d2c0326d41fc78ea4890fa16681cce
DrIrvine <72073872+MarshallIrvine@users.noreply.github.com>
2023-01-26 16:30:25 +0100

    remove unnecessary import

--- app/src/main/java/com/marshall/irvine/mybulletjournal/BulletJournalApplication.kt
+++ app/src/main/java/com/marshall/irvine/mybulletjournal/BulletJournalApplication.kt
@@ -2,7 +2,6 @@ package com.marshall.irvine.mybulletjournal
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import dagger.hilt.android.qualifiers.ApplicationContext
 
 @HiltAndroidApp
 class BulletJournalApplication: Application() {
